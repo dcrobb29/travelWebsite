d783080c270e49aeb697c1ec0c7ad643a9f7cfcc
dcrobb29 <151109837+dcrobb29@users.noreply.github.com>
2024-11-21 15:01:37 -0500

    Add files via upload

---
+++ travel_recommendation.js
@@ -0,0 +1,31 @@
+function searchLocation() {
+    const input = document.getElementById('locationInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('travel_recommendation_api.json')
+      .then(response => response.json())
+      .then(data => {
+        const location = data.locations.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const symptoms = condition.symptoms.join(', ');
+          const prevention = condition.prevention.join(', ');
+          const treatment = condition.treatment;
+
+          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        } else {
+          resultDiv.innerHTML = 'Condition not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+  }
+    btnSearch.addEventListener('click', searchCondition);
